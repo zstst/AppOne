a2998220eebd798a6d8755d3efd640158f5314b0
zstst <zstst@163.com>
2018-03-07 16:52:56 +0800

    first commit

---
+++ app/src/main/java/com/e0/appone/MainActivity.java
@@ -0,0 +1,13 @@
+package com.e0.appone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
